f70a2efb6c778a80cad45fa05d952ca24e92bd35
Oleg Repin <iamolegga@gmail.com>
2019-10-29 17:38:45 +0300

    moved boilerplate logic to `create-nestjs-middleware-module`

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-export * from './SessionModule';
-export { NestSessionAsyncOptions, NestSessionOptions } from './options';
+import {
+  AsyncOptions,
+  createModule,
+  SyncOptions,
+} from 'create-nestjs-middleware-module';
+import * as expressSession from 'express-session';
+import { createRetriesMiddleware } from './retriesMiddleware';
+
+interface Options {
+  session: expressSession.SessionOptions;
+  retries?: number;
+  retriesStrategy?: Parameters<typeof createRetriesMiddleware>[2];
+}
+
+export type NestSessionOptions = SyncOptions<Options>;
+
+export type NestSessionAsyncOptions = AsyncOptions<Options>;
+
+export const SessionModule = createModule<Options>(options => {
+  const { retries, session, retriesStrategy } = options;
+  let middleware = expressSession(session);
+
+  if (retries !== undefined) {
+    middleware = createRetriesMiddleware(middleware, retries, retriesStrategy);
+  }
+
+  return middleware;
+});
